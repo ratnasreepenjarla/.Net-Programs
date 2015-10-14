7859215e4d44dea67c98cb6b2ae7998951dfaff8
ratnasreepenjarla <ratnasree8@gmail.com>
2015-10-14 16:58:53 -0700

    XmlPrograms

---
+++ XmlDocumentWriter/XmlDocumentWriter/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlDocumentWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int choice;
+           bool iscontinue=true;
+            XmlDocument doc = new XmlDocument();
+           
+            XmlElement businfo = doc.CreateElement("buses");
+            do
+            {
+                XmlElement buselement = doc.CreateElement("bus");
+                XmlAttribute busnumattribute = doc.CreateAttribute("busnum");
+                XmlAttribute stimeattribute = doc.CreateAttribute("startingtime");
+                XmlAttribute etimeattribute = doc.CreateAttribute("departuretime");
+
+                XmlElement bookingelement = doc.CreateElement("bookingtype");
+                XmlElement costelement = doc.CreateElement("price");
+                XmlElement seatselement = doc.CreateElement("numofseats");
+
+                Console.WriteLine("enter busnum");
+                String busnum = Console.ReadLine();
+                Console.WriteLine("enter starting time");
+                String startingtime = Console.ReadLine();
+                Console.WriteLine("enter departuretime");
+                String departuretime = Console.ReadLine();
+                Console.WriteLine("enter bookintype");
+                String bookingtype = Console.ReadLine();
+
+                Console.WriteLine("enter price");
+                String price = Console.ReadLine();
+                Console.WriteLine("enter num of seats");
+                String numofseats = Console.ReadLine();
+
+
+                busnumattribute.Value = busnum;
+                stimeattribute.Value = startingtime;
+                etimeattribute.Value = departuretime;
+
+                bookingelement.InnerText = bookingtype;
+                costelement.InnerText = price;
+                seatselement.InnerText = numofseats;
+
+                buselement.Attributes.Append(busnumattribute);
+                buselement.Attributes.Append(stimeattribute);
+                buselement.Attributes.Append(etimeattribute);
+
+                buselement.AppendChild(bookingelement);
+                buselement.AppendChild(costelement);
+                buselement.AppendChild(seatselement);
+
+                businfo.AppendChild(buselement);
+                doc.AppendChild(businfo);
+                Console.WriteLine(("do you want to continue? yes=>1 , exit=>0" ));
+                 choice = int.Parse(Console.ReadLine());
+            } while (iscontinue=(choice==1));
+
+            doc.Save("businfo.xml");
+         
+        }
+    }
+}
---
+++ Xmldocument_Reader2/Xmldocument_Reader2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Xmldocument_Reader2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            Console.WriteLine("BUSESINFORMATION");
+            doc.Load("books.xml");
+            XmlNodeList buseslist = doc.SelectNodes("/buses/bus");
+           
+            foreach(XmlNode buslist in buseslist)
+            {
+                Console.WriteLine(buslist.InnerText.Trim());
+                Console.WriteLine("{0},{1},{2}",buslist.Attributes.GetNamedItem("busnum").Value,buslist.Attributes.GetNamedItem("startingtime").Value,
+                   buslist.Attributes.GetNamedItem("departuretime").Value );
+            }
+
+        }
+    }
+}
