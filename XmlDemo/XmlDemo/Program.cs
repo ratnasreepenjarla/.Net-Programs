92c3d628b80341861078a8413d5909334c88aecf
ratnasreepenjarla <ratnasree8@gmail.com>
2015-10-14 17:04:28 -0700

    XmlDemo

---
+++ XmlDemo/XmlDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReader reader = new XmlTextReader("Books.xml");
+            while(reader.Read())
+            {
+                //Console.WriteLine("Node Type={0},Name={1},value={2}",reader.NodeType,reader.Name,reader.Value);
+                Console.WriteLine("{0},{1},{2}", reader.NodeType, reader.Name, reader.Value);
+ 
+ 
+                while(reader.Name=="Books")
+                {
+                    reader.MoveToContent();
+                    reader.Read();
+                                 
+                }
+            }
+        }
+    }
+}
