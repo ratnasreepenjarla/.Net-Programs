5859ea1f8af448d331e89e156510c2b7d1af73c4
ratnasreepenjarla <ratnasree8@gmail.com>
2015-10-20 05:25:16 -0700

    comparevalidator

--- Comparevalidator/Comparevalidator/Properties/AssemblyInfo.cs
+++ Comparevalidator/Comparevalidator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmployeeRegistration")]
+[assembly: AssemblyTitle("Comparevalidator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TEAM OS")]
-[assembly: AssemblyProduct("EmployeeRegistration")]
+[assembly: AssemblyProduct("Comparevalidator")]
 [assembly: AssemblyCopyright("Copyright © TEAM OS 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a19cb18b-c9fb-4779-9b44-45f26ed7b1f1")]
+[assembly: Guid("59dfe5ef-c9ac-477e-aa4d-1464c041c830")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Comparevalidator/Comparevalidator/Validator2.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Validator2.aspx.cs" Inherits="Comparevalidator.Validator2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>
+  </title>
+   <style>
+       .labels
+       {
+           color:cyan;
+       }
+
+     table
+       {
+           background-image:url("img/Webpage_Background_by_kierun.jpg")
+       }
+   </style>
+</head>
+<body>
+    <form id="form1"  runat="server" style=" ">
+    <div>
+        <table>
+            <tr>
+                <td class="labels"> password</td>
+               <td class="labels"><asp:TextBox ID="TextBox1" runat="server" TextMode="Password"></asp:TextBox>
+                   
+                </td></tr>
+            <tr>
+                <td class="labels"> confirmpassword</td>
+                <td class="labels"><asp:TextBox ID="TextBox2" runat="server" TextMode="Password"></asp:TextBox></td>
+            </tr>  
+            <tr><td colspan="2">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<asp:Button ID="Button1" runat="server" Text="equal" /></td></tr>
+            </table>
+        <asp:CompareValidator ID="CompareValidator1" ControlToValidate="TextBox2"  ControlToCompare="TextBox1" runat="server" ErrorMessage="CompareValidator"></asp:CompareValidator>
+
+    </div>
+     
+    </form>
+</body>
+</html>
