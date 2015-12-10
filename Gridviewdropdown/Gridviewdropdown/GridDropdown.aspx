91670673ec6621b91226b51a532a6dd8275aacfc
ratnasreepenjarla <ratnasree8@gmail.com>
2015-12-10 16:21:55 +0530

    Griddropdown

---
+++ Gridviewdropdown/Gridviewdropdown/GridDropdown.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GridDropdown.aspx.cs" Inherits="Gridviewdropdown.GridDropdown" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Label ID="lblselectemp" runat="server" Text="select emp"></asp:Label>
+        <asp:DropDownList ID="DropDownList1" runat="server">
+        </asp:DropDownList>
+        <p>
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+        </p>
+        <asp:Label ID="lblmsg" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Gridviewdropdown/Gridviewdropdown/GridDropdown.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Gridviewdropdown
+{
+    public partial class GridDropdown : System.Web.UI.Page
+    {
+       String str=ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           if(!IsPostBack)
+           {
+               bind();
+           }
+           
+        }
+        public void bind()
+        {
+            SqlConnection con = new SqlConnection(str);
+            SqlCommand cmd = new SqlCommand("select * from Employees", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "employees");
+            DropDownList1.DataSource = ds;
+            DropDownList1.DataTextField = "name";
+            DropDownList1.DataBind();
+
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from Employees where name=@ename", con);
+            String name1 = DropDownList1.SelectedItem.ToString();
+            cmd.Parameters.AddWithValue("ename",name1);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            lblmsg.Text = "record is deleted";
+            bind();
+        }
+    }
+}
+
+
